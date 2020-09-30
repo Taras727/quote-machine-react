4893e3741b14673230c6ef5afb3b933adf63014a
Taras Shevchuk <tara7777@ukr.net>
2020-09-30 13:23:47 +0300

    Updates

---
+++ precache-manifest.c2c6ae788ae50918bc4d5b5ccdbab912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f770c593c687020d591ee6e8666ba434",
+    "url": "/quote-machine-react/index.html"
+  },
+  {
+    "revision": "7bf83ae6785bcd3fc5d2",
+    "url": "/quote-machine-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f212e03ee15ca6e40f5f",
+    "url": "/quote-machine-react/static/js/2.2d5dbb37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine-react/static/js/2.2d5dbb37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf83ae6785bcd3fc5d2",
+    "url": "/quote-machine-react/static/js/main.395203ef.chunk.js"
+  },
+  {
+    "revision": "0b3e1db8d7c5ddfe28d0",
+    "url": "/quote-machine-react/static/js/runtime-main.51747f16.js"
+  }
+]);
